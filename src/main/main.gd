d3cea267cbd9b433ffc4331bd9cb31a38d6ce1f5
sebashtioon <sebastiansuciu607@gmail.com>
2025-12-17 18:59:05 +1100

    breathe out

--- src/main/main.gd
+++ src/main/main.gd
@@ -20,8 +20,8 @@ func _process(_delta: float) -> void:
 	$infolbls/breath_interval.text = "breath interval: " + str($Timers/breath_interval.wait_time)
 	$infolbls/breath_accuracy_gap.text = "breath accuracy gap: " + str($Timers/breath_accuracy_cap.wait_time)
 	
-	$infolbls/breath_accuracy_gap_left.text = "breath accuracy gap time left: " + str($Timers/breath_accuracy_cap.time_left)
-	$infolbls/breath_interval_left.text = "breath interval time left: " + str($Timers/breath_interval.time_left)
+	$infolbls/breath_accuracy_gap_left.text = "breath accuracy gap time left: " + "%.2f" % $Timers/breath_accuracy_cap.time_left
+	$infolbls/breath_interval_left.text = "breath interval time left: " + "%.2f" % $Timers/breath_interval.time_left
 	
 	$infolbls/first_breath.text = "first breath? " + str(first_breath)
 	
@@ -40,6 +40,15 @@ func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("input") and first_breath:
 		first_breath = false
 		$Timers/breath_interval.start()
+	
+	
+	if Input.is_action_just_released("input"):
+		if $Timers/breath_accuracy_cap.time_left > 0.0:
+			print("hoorah")
+			$Timers/breath_accuracy_cap.stop()
+			$Timers/breath_interval.start()
+		else:
+			print("goober")
 
 
 
